d0c57085999e261d1c5ec9b0c464e2e3ed304877
abc <321>
2024-01-14 14:38:04 +0500

    Assignment # 7

--- todoApp/src/Input.jsx
+++ todoApp/src/Input.jsx
@@ -5,7 +5,7 @@ const Input = (props) => {
     return (
       <div className="input-container">
         <input id={props.id} type={props.type} placeholder={props.placeholder}
-               onChange={(e) => props.onChangeHandle(e.target.value, props.id)} />
+               onChange={(e) => props.onChangeHandle(e.target.value, props.id)}  value={props.value}/>
           <label htmlFor="myInput">{props.placeholder}</label>
       </div>
     )
